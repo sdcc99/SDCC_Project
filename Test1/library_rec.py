84dedc2c2661682d2763c0de91a52012e7511aab
sdcc99 <126062386+sdcc99@users.noreply.github.com>
2023-02-22 11:18:10 +0100

    Add files via upload

---
+++ Test1/library_rec.py
@@ -0,0 +1,62 @@
+import face_recognition as fr
+import cv2
+import numpy as np
+import os
+
+path = "static/train/"
+
+known_names = []
+known_name_encodings = []
+
+def recognition():
+    images = os.listdir(path)
+
+    #print(images)
+        
+    for _ in images:
+        #print(path + _)
+        if not _.startswith('.'):
+            image = fr.load_image_file(path + _)
+            image_path = path + _
+            encoding = fr.face_encodings(image)[0]
+            
+            #print(encoding)
+
+            known_name_encodings.append(encoding)
+            known_names.append(os.path.splitext(os.path.basename(image_path))[0].capitalize())
+
+
+    #print(known_names)
+
+    test_image = "static/uploads/imageP.png"
+    image = cv2.imread(test_image)
+    # image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+
+    face_locations = fr.face_locations(image)
+    face_encodings = fr.face_encodings(image, face_locations)
+
+    for (top, right, bottom, left), face_encoding in zip(face_locations, face_encodings):
+        matches = fr.compare_faces(known_name_encodings, face_encoding)
+        name = ""
+
+        face_distances = fr.face_distance(known_name_encodings, face_encoding)
+        best_match = np.argmin(face_distances)
+
+        if matches[best_match]:
+            name = known_names[best_match]
+
+        cv2.rectangle(image, (left, top), (right, bottom), (0, 0, 255), 2)
+        cv2.rectangle(image, (left, bottom - 15), (right, bottom), (0, 0, 255), cv2.FILLED)
+        font = cv2.FONT_HERSHEY_DUPLEX
+        cv2.putText(image, name, (left + 6, bottom - 6), font, 1.0, (255, 255, 255), 1)
+
+
+    #cv2.imshow("Result", image)
+    percorso = "static/uploads/imageP2.png"
+    cv2.imwrite(percorso, image)
+    return percorso
+    #cv2.waitKey(0)
+    #cv2.destroyAllWindows()
+
+
+
--- Test1/server.py
+++ Test1/server.py
@@ -16,6 +16,7 @@
 
 from flask import Flask, request, render_template, send_file, redirect, url_for
 import os
+import library_rec as rec
 
 app = Flask(__name__, static_url_path='/static')
 
@@ -23,25 +24,25 @@ app.config['UPLOAD_FOLDER'] = 'static/uploads'
 
 @app.route('/')
 def index():
-    #return render_template('index.html')
-    # ottieni una lista di tutti i file presenti nella cartella "uploads"
-    files = [f for f in os.listdir(app.config['UPLOAD_FOLDER']) if not f.startswith('.') and not f.endswith('.DS_Store')]
+    # #return render_template('index.html')
+    # # ottieni una lista di tutti i file presenti nella cartella "uploads"
+    # files = [f for f in os.listdir(app.config['UPLOAD_FOLDER']) if not f.startswith('.') and not f.endswith('.DS_Store')]
     
-    # crea una lista di card Bootstrap per ogni file presente nella cartella "uploads"
-    cards = []
-    for file in files:
-        card = """
-        <div class="card" style="width: 18rem;">
-            <img src="{{ url_for('static', filename='uploads/%s') }}" class="card-img-top" alt="...">
-            <div class="card-body">
-                <p class="card-text">%s</p>
-            </div>
-        </div>
-        """ % (file, file)
-        cards.append(card)
+    # # crea una lista di card Bootstrap per ogni file presente nella cartella "uploads"
+    # cards = []
+    # for file in files:
+    #     card = """
+    #     <div class="card" style="width: 18rem;">
+    #         <img src="{{ url_for('static', filename='uploads/%s') }}" class="card-img-top" alt="...">
+    #         <div class="card-body">
+    #             <p class="card-text">%s</p>
+    #         </div>
+    #     </div>
+    #     """ % (file, file)
+    #     cards.append(card)
     
     # restituisci la pagina HTML con tutte le card Bootstrap
-    return render_template('index.html', cards=cards)
+    return render_template('index.html')    #cards=cards
 
 @app.route('/upload', methods=['GET', 'POST'])
 def upload_file():
@@ -49,8 +50,9 @@ def upload_file():
         file = request.files['file']
         filename = file.filename
         file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+        
         #return 'file uploaded successfully'
-        return filename
+        return rec.recognition()
         #return redirect(url_for('index'))
     #return render_template('index.html', cards=cards)
 
@@ -62,6 +64,7 @@ def uploaded_file(filename):
 def images():
     files = os.listdir(app.config['UPLOAD_FOLDER'])
     images = [f for f in files if f.endswith(('.jpg', '.jpeg', '.png', '.gif'))]
+    #print(','.join(images))
     return ','.join(images)
 
 
